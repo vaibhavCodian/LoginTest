d28791e8195d21915e3addc3beb4a10091815ab8
Vaibhav <vaibhavshukla.try@gmail.com>
2022-11-30 15:49:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Msg(BaseModel):
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+    return {"message": "Hello Vaibhav. Welcome to FastAPI!"}
 
 
 @app.get("/path")
