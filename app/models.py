c09d25532cd3d03a54d82b7bc841dce4a6f270a1
vaibhav <vaibhavshukla.try@gmail.com>
2022-12-27 20:07:12 +0530

    --Initial Login

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Numeric
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
@@ -20,7 +20,8 @@ class Purchases(Base):
     __tablename__ = "purchases"
     id = Column(Integer, primary_key=True, nullable=False)
     ProductName = Column(String, nullable=False)
-    Price = Column(String, nullable=False)
+    Quantity = Column(Integer, nullable=False)
+    Price = Column(Numeric, nullable=False)
     purchased_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
     owner_id = Column(Integer, ForeignKey("customers.id", ondelete="CASCADE"), nullable=False)
@@ -31,7 +32,9 @@ class Customers(Base):
     __tablename__ = "customers"
     id = Column(Integer, primary_key=True, nullable=False)
     Name = Column(String, nullable=False)
+    Email = Column(String, nullable=True)
     Age = Column(Integer, nullable=False)
     Gender = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True),
+    Created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
+    Catagory = Column(Integer, nullable=True)
